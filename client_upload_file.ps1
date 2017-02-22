164ece5f019930a7f0eece08b8f34eda4922da9d
Or Ohev Zion <or.ohev.zion@gmail.com>
2017-02-22 15:31:50 +0200

    Update to client script
    Added ability to scan executable in client script

--- client_upload_file.ps1
+++ client_upload_file.ps1
@@ -10,24 +10,42 @@ Function LogWrite
 # Create the WebClient that makes the post request
 $WebClient = new-object System.Net.WebClient
 
+# Ask user to check highest cpu process executable, or input a file
+$ReportType = Read-Host -Prompt 'Scan High CPU Process Executable or a specific file? 0 for Process, 1 for file: '
+if ($ReportType -eq 0){
+    $ProcessPathObject = Get-Process | Sort CPU -descending | Select -first 1 | Select-Object Path
+    $FilePath = $ProcessPathObject.Path
+    Write-Host "Scanning executable: $FilePath"
+    LogWrite "Scanning executable: $FilePath"
+} elseif ($ReportType -eq 1){
+    $FileName = Read-Host -Prompt 'Input file name: '
+    $FilePath = $PSScriptRoot + "\" + $FileName
+    Write-Host "Scanning input file: $FilePath"
+    LogWrite "Scanning input file: $FilePath"
+} else {
+    Write-Host "Unknown option, terminating..."
+    exit
+}
+
 # Prepare the paramaters for the post request
 $Uri = "http://localhost:8080/"
-$fileName = Read-Host -Prompt 'Input file name: '
+# $fileName = Read-Host -Prompt 'Input file name: '
 
 # Prepare full path ofthe input file
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-$filePath = $scriptPath + "\" + $fileName
+# $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+# $filePath = $scriptPath + "\" + $fileName
 
 # Print to console file name and server address
-LogWrite "Uploading $fileName to $Uri"
+LogWrite "Uploading $FilePath to $Uri"
 
 # Send the post request and save response
-$Response = $WebClient.UploadFile($Uri,$filePath)
+$Response = $WebClient.UploadFile($Uri,$FilePath)
 
 # Save Json response
 $JsonResponse = [System.Text.Encoding]::ASCII.GetString($Response) | ConvertFrom-Json
 $PositiveInfections = $JsonResponse.positives
 $ScanID = $JsonResponse.scan_id
+
 LogWrite "Scan ID: $ScanID"
 Write-Host "Scan ID: $ScanID"
 LogWrite "Number of infections found by VirusTotal: $PositiveInfections"
--- find_suspected_process.ps1
+++ find_suspected_process.ps1
@@ -1,2 +1,1 @@
-$process = Get-Process | Sort CPU -descending | Select -first 5 -Property ID,ProcessName,CPU | format-table -autosize
-$process
+$ProcessPath = Get-Process | Sort CPU -descending | Select -first 1 | Select-Object Path
